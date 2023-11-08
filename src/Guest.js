81ba2ee8e241fb5e4c77017b025d2193efb973cd
JaNeTahil <143070441+JaNeTahil@users.noreply.github.com>
2023-11-08 16:08:23 +0800

    Srollview

---
+++ src/Guest.js
@@ -0,0 +1,82 @@
+import react from "react";
+
+export const guest = [
+    {
+        id: 1,
+        name: "TAHIL, ADRIAN JANE",
+        place: "Thailand, Bangkok"
+    },
+    {
+        id: 2,
+        name: "PAGUICAN, NICOLE JAMES",
+        place: "Korea, Seoul"
+    },
+    {
+        id: 3,
+        name: "GARCIA, RODEL",
+        place: "United Kingdom "
+    },
+    {
+        id: 3,
+        name: "GARCIA, ROGEL",
+        place: "Japan, Tokyo"
+    },
+    {
+        id: 3,
+        name: "JAGUNOS, JOANA",
+        place: "China "
+    },
+    {
+        id: 3,
+        name: "REQUILLO, FLORA MAE",
+        place: "Hongkong"
+    },
+    {
+        id: 3,
+        name: "CARBONILLA, MARLON",
+        place: "Switzerland "
+    },
+    {
+        id: 3,
+        name: "SARABIA, EARL MIKE",
+        place: "Taiwan"
+    },
+    {
+        id: 3,
+        name: "LIPARTO, MONALIZA",
+        place: "India"
+    },
+    {
+        id: 3,
+        name: "FELISILDA, ARNOLD JAMES",
+        place: "Spain"
+    },
+    {
+        id: 3,
+        name: "SARAGA, SHIELO",
+        place: "Rome, Italy"
+    },
+    {
+        id: 3,
+        name: "DONDOYANO, SHERLINE",
+        place: "Korea, Seoul"
+    },
+    {
+        id: 3,
+        name: "BONGCARAS, AIREEN MAE",
+        place: "United States of America"
+    },
+    {
+        id: 3,
+        name: "TINAJA, MA. LUISA",
+        place: "Taiwan"
+    },
+    {
+        id: 3,
+        name: "ORING, JEA NORENE",
+        place: "Cambodia"
+    },
+
+]
+
+export default guest;
